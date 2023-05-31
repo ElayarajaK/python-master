5e974dcf1540aaf99b0792ede43f93d507864165
Raja <kelaya5@gmail.com>
2023-05-31 08:09:15 +0530

    commit

---
+++ 31-05/App1.py
@@ -0,0 +1,34 @@
+'''
+    int
+    ===================
+    decimals
+    binaryvalues 0b0110101
+    octal values 0o01234567
+    hexadecimal value 0x0123456789abcdef
+'''
+i= bin(10);
+binayvalues=0b1010;
+octalValue=0o0123543254325634523251234544567;
+
+
+hexadecimal=0x0123456789abcdef;
+
+print(type(hexadecimal))
+print(hexadecimal)
+print(octalValue)
+name="raja"
+isAdmin=True;
+
+print(i);
+print(type(binayvalues))
+
+print(type(i))
+print(type(name))
+print(type(isAdmin))
+
+#()->perathisis  ()methods
+#{}-> braces
+#[]->brackets
+
+# 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+# 0 1 2 3 4 5 6 7 8 9   A  B  C  D  E  F
---
+++ 31-05/App2.py
@@ -0,0 +1,22 @@
+#str  --> String
+'''
+multi 
+line commandts
+'''
+name="Python's";
+Address='this is the address chennai"s ';
+Details='''i am raja's
+and
+i am 
+from chennai"s
+''';
+print(name)
+print(Address)
+print(Details)
+print(type(name))
+print(type(Address))
+print(type(Details))
+print("i am python",name,Address,Details,"End")
+
+print(10+10)
+print("10"+"10")
